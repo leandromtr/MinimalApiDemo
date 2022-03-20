0ddb8dc42673603cd2609a30bad26d29cf6f1937
Leandro Reis <leandro.reis@nextbitt.com>
2022-03-20 18:00:13 +0000

    Add Initial Setup

---
+++ MinimalApiDemo/Data/MinimalContextDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiDemo.Models;
+
+namespace MinimalApiDemo.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options) : base(options) { }
+
+        public DbSet<Provider> Providers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Provider>()
+                .HasKey(prop => prop.Id);
+
+            modelBuilder.Entity<Provider>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Provider>()
+                .Property(p => p.Document)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            modelBuilder.Entity<Provider>()
+                .ToTable("Providers");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- MinimalApiDemo/Program.cs
+++ MinimalApiDemo/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApiDemo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
