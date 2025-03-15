b6dc6b30c9aa36f95b75a5bab9fc7797745ebf0c
Leandro Reis <leandro.reis@nextbitt.com>
2025-03-15 21:20:38 +0000

    feat: Add project DishesAPI

---
+++ DishesAPI/Program.cs
@@ -0,0 +1,54 @@
+using DishesAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<DishesDbContext>(o => o.UseSqlite(
+    builder.Configuration["ConnectionStrings:DishesDBConnectionString"]));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+//var dish = new Dish
+//{
+//    Name = "Biryani",
+//    Description = "Biryani is a mixed rice dish originating among the Muslims of the Indian subcontinent.",
+//    Price = 10.5m
+//};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<DishesDbContext>();
+    context.Database.EnsureDeleted();
+    context.Database.Migrate();
+}
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
