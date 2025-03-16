0c4c0cd2c7610e20cee904274debf0d03fda4676
Leandro Reis <leandro.reis@nextbitt.com>
2025-03-16 01:06:03 +0000

    feat: First EndPoints

--- DishesAPI/Program.cs
+++ DishesAPI/Program.cs
@@ -1,4 +1,8 @@
+using AutoMapper;
 using DishesAPI.DbContexts;
+using DishesAPI.Entities;
+using DishesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,36 +12,42 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DishesDbContext>(o => o.UseSqlite(
     builder.Configuration["ConnectionStrings:DishesDBConnectionString"]));
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-//var dish = new Dish
+//app.MapGet("/dishes", (DishesDbContext dishesDbContext) =>
 //{
-//    Name = "Biryani",
-//    Description = "Biryani is a mixed rice dish originating among the Muslims of the Indian subcontinent.",
-//    Price = 10.5m
-//};
+//    return dishesDbContext.Dishes;
+//});
 
-app.MapGet("/weatherforecast", () =>
+//async
+app.MapGet("/dishes", async (DishesDbContext dishesDbContext, IMapper mapper, [FromQuery] string? name) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return mapper.Map<IEnumerable<DishDto>> (await dishesDbContext.Dishes.ToListAsync());
 });
+
+app.MapGet("/dishes/{dishId:guid}", async (DishesDbContext dishesDbContext, IMapper mapper, Guid dishId) =>
+{
+    return mapper.Map<DishDto>(await dishesDbContext.Dishes.FirstOrDefaultAsync(d=> d.Id == dishId));
+});
+
+app.MapGet("/dishes/{dishName}", async (DishesDbContext dishesDbContext, IMapper mapper, string dishName) =>
+{
+    return mapper.Map<DishDto>(await dishesDbContext.Dishes.FirstOrDefaultAsync(d => d.Name == dishName));
+});
+
+app.MapGet("/dishes/{dishId}/ingredients", async (DishesDbContext dishesDbContext, IMapper mapper, Guid dishId) =>
+{
+    return mapper.Map<IEnumerable<IngredientDto>>((await dishesDbContext.Dishes
+        .Include(d => d.Ingredients)
+        .FirstOrDefaultAsync(d => d.Id == dishId))?.Ingredients);
+});
+
 
 using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
 {
@@ -47,8 +57,3 @@ using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().Create
 }
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
